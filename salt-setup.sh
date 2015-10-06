dda31a238e2bae13ce7e748389ed3162976e4745
Devon Stewart <blast@hardchee.se>
2015-10-06 15:14:19 -0700

    Adding CentOS 6 support to salt installer

--- salt-setup.sh
+++ salt-setup.sh
@@ -17,6 +17,28 @@ gpgcheck=1
 gpgkey=https://repo.saltstack.com/yum/rhel7/SALTSTACK-GPG-KEY.pub
 !
 
+    yum clean -y expire-cache
+    yum install -y epel-release vim
+    yum install -y salt-master salt-minion git python-pygit2
+
+if [ -f /etc/redhat-release ] && grep "release 6\." /etc/redhat-release >/dev/null; then
+    target=/tmp/SALTSTACK-GPG-KEY.pub
+    curl -o "$target" https://repo.saltstack.com/yum/rhel6/SALTSTACK-GPG-KEY.pub
+    rpm --import "$target"
+    rm -f "$target"
+
+    cat >/etc/yum.repos.d/saltstack.repo <<!
+####################
+# Enable SaltStack's package repository
+[saltstack-repo]
+name=SaltStack repo for RHEL/CentOS 6
+baseurl=https://repo.saltstack.com/yum/rhel6
+enabled=1
+gpgcheck=1
+gpgkey=https://repo.saltstack.com/yum/rhel6/SALTSTACK-GPG-KEY.pub
+!
+
+    yum clean -y expire-cache
     yum install -y epel-release vim
     yum install -y salt-master salt-minion git python-pygit2
 else
